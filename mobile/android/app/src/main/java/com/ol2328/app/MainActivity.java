6d8b0e72cc4d57c9b186c102fb8fca089afde6f1
CSharpProjector <marleymulvinbroome@gmail.com>
2023-10-28 10:22:41 +0900

    モバイル環境を初期化

---
+++ mobile/android/app/src/main/java/com/ol2328/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ol2328.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ol2328.app',
+  appName: 'nottapple',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
