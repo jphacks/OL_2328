16a2d9ac58b500bf9d78632f53d51dbcc1a432d5
CSharpProjector <marleymulvinbroome@gmail.com>
2023-10-28 17:42:52 +0900

    簡単なユーザー認証

--- mobile/src/routes/login/+page.svelte
+++ mobile/src/routes/login/+page.svelte
@@ -1,30 +1,42 @@
 <script lang="ts">
+	import Redirect from '$lib/components/Redirect.svelte';
+import { signInWithEmailAndPassword, type Auth } from 'firebase/auth';
     import { Block, BlockTitle, Button, List, ListInput, ListItem, Navbar, Page, Radio, Toggle } from 'konsta/svelte';
+	import { SignedIn, SignedOut } from 'sveltefire';
+    
+    let email = "";
+    let password = "";
+
+    const login = (auth: Auth) => {
+        signInWithEmailAndPassword(auth, email, password);
+    }
 </script>
 
-<Page>
-    <BlockTitle class="text-5xl text-center justify-self-center" component="h1">
-        Login
-    </BlockTitle>
+<SignedIn>
+    <Redirect to="/main" />
+</SignedIn>
 
-    <List>
-        <ListInput label="メール" type="email">
-            <iconify-icon width="25" height="25" slot="media" icon="ic:baseline-email"></iconify-icon>
-        </ListInput>
-        <ListInput label="パスワード" type="password">
-            <iconify-icon width="25" height="25" slot="media" icon="ic:baseline-lock"></iconify-icon>
-        </ListInput>
-
-    </List>
-
-    <Block>
-        <div class="flex flex-col gap-2">
-            <Button rounded>ログイン</Button>
-            <Button rounded outline>新規登録</Button>
-        </div>
-        
-    </Block>
-
-
-
-</Page>
+<SignedOut let:auth>
+    <Page>
+        <BlockTitle class="text-5xl text-center justify-self-center" component="h1">
+            Login
+        </BlockTitle>
+    
+        <List>
+            <ListInput bind:value={email} label="メール" type="email">
+                <iconify-icon width="25" height="25" slot="media" icon="ic:baseline-email"></iconify-icon>
+            </ListInput>
+            <ListInput bind:value={password} label="パスワード" type="password">
+                <iconify-icon width="25" height="25" slot="media" icon="ic:baseline-lock"></iconify-icon>
+            </ListInput>
+    
+        </List>
+    
+        <Block>
+            <div class="flex flex-col gap-2">
+                <Button rounded on:onclick={() => { login(auth); }}>ログイン</Button>
+                <Button rounded outline>新規登録</Button>
+            </div>
+        </Block>
+    </Page>
+</SignedOut>
